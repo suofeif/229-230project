984a7b11f921d36bc88462e2d41a9f4998544ffe
ed173 <45318491+ed173@users.noreply.github.com>
2018-11-30 16:33:35 -0800

    Update Gradient Boosting.py

--- Gradient Boosting.py
+++ Gradient Boosting.py
@@ -1,14 +1,21 @@
 import numpy as np
+import config
+import os
+import logging
 from numpy import array
 import numpy as np
 import scipy as sp
 import xgboost as xgb
 from sklearn import datasets
 from sklearn.metrics import confusion_matrix
-from sklearn.grid_search import GridSearchCV
-from sklearn.grid_search import RandomizedSearchCV
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+import config
+import os
+import logging
+from pre_processing_ref import load_data_from_csv, seg_words, train_vec, train_tencent_model, convert_to_onehot, sentence_to_indice, embedding_data, wordToIndex
+
+
 # define documents
 train=load_data_from_csv(config.train_data_path)
 val = load_data_from_csv(config.validate_data_path)
@@ -23,9 +30,10 @@ logger = logging.getLogger(__name__)
 #train_doc = train[:, 1]
 #val_doc = val[0:7500, 1]
 #test_doc = val[7501:m_val-1, 1]
-train_doc = train.iloc[1:10, 1]
-val_doc = val.iloc[0:5, 1]
-test_doc = val.iloc[6:10, 1]
+m_val = val.shape[0]
+train_doc = train.iloc[:, 1]
+val_doc = val.iloc[:, 1]
+test_doc = val.iloc[7501:m_val-1, 1]
 print("finish splitting")
 print(train_doc.shape)
 print(val_doc.shape)
@@ -35,18 +43,18 @@ print(test_doc.shape)
 #train_labels = array(train[:, 2:])
 #val_labels = array(val[0:7500, 2:])
 #test_labels = array(val[7501:m_val-1, 2:])
-train_labels = array(train.iloc[1:10, 2:])
-val_labels = array(val.iloc[0:5, 2:])
-test_labels = array(val.iloc[6:10, 2:])
+train_labels = array(train.iloc[:, 2:])
+val_labels = array(val.iloc[:, 2:])
+test_labels = array(val.iloc[:, 2:])
 #print("test number"+str(test_labels.shape[0]))
 print("finish loading labels")
 print(train_labels.shape)
 print(val_labels.shape)
 print(test_labels.shape)
 
-train_indices = np.load("train_indices.dat")
-val_indices = np.load("val_indices.dat")
-test_indices = np.load("test_indices.dat")
+train_indices = np.load("/Users/EzizDurdyev/Downloads/train_indices.dat")
+val_indices = np.load("/Users/EzizDurdyev/Downloads/val_indices.dat")
+test_indices = np.load("/Users/EzizDurdyev/Downloads/test_indices.dat")
 max_length=train_indices.shape[1]
 print("max_length")
 print(max_length)
@@ -58,7 +66,7 @@ params = {'learning_rate': [0.05, 0.1],
 
 clf = xgb.XGBClassifier()
 
-cv = GridSearchCV(clf, params, cv = 10, scoring = "log_loss", n_jobs = -1, verbose = 2)
+cv = GridSearchCV(clf, params, cv = 10, scoring = "f1", n_jobs = -1, verbose = 2)
 
 cv.fit(train_indices, train_labels)
 predict = cv.predict(val_indices)
@@ -89,4 +97,3 @@ for i,v in enumerate(predict):
     if np.argmax(v) == val_labels[i]:
         score += 1
 print ("accuracy is {acc}".format(acc = score/total_num))
-
